e4a6afff982fd9f74c185429c18e1d8d12c7abe2
An, Albert <alban@visa.com>
2016-12-08 13:23:23 -0800

    push for test without yml yet

---
+++ app/src/main/java/test/circle/skeleton/visa/sms/phone/mainskel_test/MainActivity.java
@@ -0,0 +1,13 @@
+package test.circle.skeleton.visa.sms.phone.mainskel_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
